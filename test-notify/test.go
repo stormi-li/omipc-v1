8eba28a53f228090f20ca9989d2860c0eebac9a8
Your Name <your.email@example.com>
2024-11-22 11:24:55 +0800

    add listener to manage listen func

--- test-notify/test.go
+++ test-notify/test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"strconv"
+	"time"
 
 	"github.com/go-redis/redis/v8"
 	omipc "github.com/stormi-li/omipc-v1"
@@ -12,8 +13,9 @@ var password = "12982397StrongPassw0rd"
 
 func main() {
 	omipc := omipc.NewClient(&redis.Options{Addr: redisAddr, Password: password})
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 100000; i++ {
 		omipc.Notify("channel", strconv.Itoa(i))
+		time.Sleep(10*time.Microsecond)
 	}
 	omipc.Notify("channel", "close")
 	c:=redis.NewClient(&redis.Options{Addr: redisAddr, Password: password})
